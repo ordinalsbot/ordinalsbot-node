68f0869c3db750de092ee3841188e45e593af802
T1-O <7LittleBoxes@gmail.com>
2024-03-06 21:17:00 -0500

    Updated description of reList test case

--- test/marketplace/reListing.js
+++ test/marketplace/reListing.js
@@ -4,8 +4,8 @@ const { MarketPlace } = require('../../dist')
 const { WALLET_PROVIDER } = require('../../dist/types/marketplace_types')
 
 const sandbox = sinon.createSandbox()
-describe('List ordinal for sale with walletProvider', function () {
-  it('should handle the listing process with a walletProvider', async () => {
+describe('ReList ordinal', function () {
+  it('should handle the relisting process', async () => {
     const marketPlace = new MarketPlace('someApiKey', 'dev')
     const createListingStub = sandbox.stub(marketPlace, 'reListing').resolves({
       psbt: 'test_psbt',
