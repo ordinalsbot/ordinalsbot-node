42c8a95c8f009839182731c54f1c8bae8b1ef9b2
Samuel Alarco <sam.alarco@gmail.com>
2024-05-14 14:37:40 +0100

    added mempool and marketplace L402 support

--- src/marketplaceClient.ts
+++ src/marketplaceClient.ts
@@ -28,40 +28,56 @@ import {
   MarketplaceDeListRequest,
   MarketplaceDeListAPIResponse,
 } from "./types/marketplace_types";
-import { InscriptionEnv } from "./types";
+import { ClientOptions, InscriptionEnv } from "./types";
+import { setupL402Interceptor } from "l402";
 
 export class MarketPlaceClient {
   public env: InscriptionEnv;
   private api_key: string;
   private instanceV1: AxiosInstance;
 
-  constructor(key: string = "", environment: InscriptionEnv = "live") {
+  /**
+   * Creates a new SatscannerClient instance.
+   * @param {string} [key=''] - The API key for authentication.
+   * @param {InscriptionEnv} [environment='live'] - The environment (live or dev) (optional, defaults to live).
+   * @param {ClientOptions} [options] - Options for enabling L402 support.
+   */
+  constructor(key: string = "", environment: InscriptionEnv = "live", options?: ClientOptions) {
     this.api_key = key;
     this.env = environment;
 
+    /**
+     * Creates a new Axios instance with appropriate headers.
+     * @returns {AxiosInstance} The new Axios instance.
+     */
     const createInstance = (): AxiosInstance => {
-
       const headers: Record<string, string> = {
         Connection: "Keep-Alive",
         "Content-Type": "application/json",
       };
-      
+
       // Add the API key header only if this.api_key has a value
       if (this.api_key) {
         headers["x-api-key"] = this.api_key;
       }
-      
+
+      // Choose the base URL based on whether L402 is used or not
+      const baseURL = options?.useL402
+        ? "https://ordinalsbot.ln.sulu.sh/marketplace/"
+        : this.env === "live"
+          ? "https://api.ordinalsbot.com/marketplace/"
+          : "https://testnet-api.ordinalsbot.com/marketplace/";
+
+      // Create the Axios client with the appropriate base URL
       const client = axios.create({
-        baseURL: this.env === "live"
-          ? `https://api.ordinalsbot.com/marketplace/`
-          : `https://testnet-api.ordinalsbot.com/marketplace/`,
-        headers: headers
+        baseURL,
+        headers: headers,
       });
-      
+
       client.interceptors.response.use(
         ({ data }) => ("data" in data ? data.data : data),
         (err) => {
-          if (axios.isAxiosError(err)) {
+          if (axios.isAxiosError(err) && err.response?.status !== 402) { // avoid modifying L402 errors.
             throw new InscriptionError(
               err.message,
               err.response?.statusText,
@@ -75,9 +91,15 @@ export class MarketPlaceClient {
         }
       );
 
+      // If L402 is enabled and configuration is provided, set up the L402 interceptor
+      if (options?.useL402 && options.l402Config) {
+        setupL402Interceptor(client, options.l402Config.wallet, options.l402Config.tokenStore);
+      };
+
       return client;
     };
 
+    // Create the Axios instance
     this.instanceV1 = createInstance();
   }
 
